8ad7ac922a2d5db8fdf9d2e274a9f986f9a4761e
ibs12 <89870752+ibs12@users.noreply.github.com>
2024-03-28 16:43:40 -0400

    modified:   backend/db_config.php

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,16 +1,9 @@
 <?php
-<<<<<<< HEAD
-$servername = "oceanus.cse.buffalo.edu";
-$username = "iallahbu";
-$password = "50344145";
-$dbname = "cse442_2024_spring_team_ac_db";
-=======
 
 $servername = ""; // enter servername
 $username = ""; // enter ubit
 $password = ""; // enter UB ID number
 $dbname = ""; // enter database name
->>>>>>> b533b8e7dd196bc46e2b45c4d675f03fefe0ed5f
 
 // Prevent XSS attacks
 header('X-XSS-Protection: 1; mode=block');
