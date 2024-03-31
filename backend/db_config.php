7d690f486b4da78455a59883daac905d07f5f2fd
Ibrahim Allahbukhsh <ibrahimallahb@gmail.com>
2024-03-31 14:59:58 -0400

    Update db_config.php

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "oceanus.cse.buffalo.edu";
-$username = "iallahbu";
-$password = "50344145";
+$username = "";
+$password = "";
 $dbname = "cse442_2024_spring_team_ac_db";
 
 // Prevent XSS attacks
