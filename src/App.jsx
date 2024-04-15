e1eedbbfff11aaa0f0c535495f9a31688d91e9aa
jiewen <jiewenhu@buffalo.edu>
2024-04-14 20:50:14 -0400

    Updated the following jsx file so it can accept guests.

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,13 @@ function App() {
             <Route path="/" element={<StartPage />} /> 
             {/* Wrap protected routes with ProtectedRoute component */}
             <Route path="/review/:name" element={<ProtectedRoute><Review /></ProtectedRoute>} />
-            <Route path="/homepage" element={<ProtectedRoute><Homepage /></ProtectedRoute>} />
+            <Route path="/homepage" element={<Homepage />} />
             <Route path="/accountsettings" element={<ProtectedRoute><AccountSettings /></ProtectedRoute>} />
             <Route path="/professorPage" element={<ProtectedRoute><ProfessorPage /></ProtectedRoute>} />
             <Route path="/quizPage" element={<ProtectedRoute><QuizPage /></ProtectedRoute>} />
-            <Route path="/professor/:name" element={<ProtectedRoute><ProfessorPage /></ProtectedRoute>} />
+            <Route path="/professor/:name" element={<ProfessorPage />} />
             {/* Assuming you want the search feature open to everyone in sprint 2, so it's not wrapped */}
-            <Route path="/search" element={<ProtectedRoute><Search /></ProtectedRoute>} />
+            <Route path="/search" element={<Search />} />
             <Route path="/saved" element={<ProtectedRoute><Saved /></ProtectedRoute>} />
             <Route path="/Recommendpage" element={<ProtectedRoute><Recommend /></ProtectedRoute>} />
             
