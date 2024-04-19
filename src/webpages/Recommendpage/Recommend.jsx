23a84c66a428205869885f0bf89556b8ad5b3ceb
Reayoal <yzhu53@buffalo.edu>
2024-04-18 23:49:50 -0400

    Redirect recommend page to professor profile
    This update enhances user experience by directly guiding users from the recommend page to the respective professor profile. By eliminating an extra step, it streamlines the process, reducing user effort and enhancing efficiency in accessing professor profiles for recommendations.

--- src/webpages/Recommendpage/Recommend.jsx
+++ src/webpages/Recommendpage/Recommend.jsx
@@ -1,87 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Default from '../../images/defaultPic.png';
-import './Recommend.css'
+import { useNavigate } from 'react-router-dom';
+import './Recommend.css';
 import NavBar from '../navBar/NavBar';
 
+const apiUrl = "http://localhost:8000"; // Assuming your backend runs on localhost port 8000
+
+const importProfessorImage = (imagePath) => {
+    try {
+      const images = require.context('../../images/professorpfp', false, /\.(png|jpe?g|svg)$/);
+      return images(`./${imagePath}`);
+    } catch (error) {
+      return Default;
+    }
+  };
+
 const RecommendedProfessorsPage = () => {
+    const navigate = useNavigate();
     const [selectedDepartment, setSelectedDepartment] = useState('');
+    const [professorsData, setProfessorsData] = useState([]);
+    const [quizPromptVisible, setQuizPromptVisible] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userID = localStorage.getItem('userID');
+                console.log('User ID sent to backend:', userID);
+
+                const response = await fetch(`${apiUrl}/backend/recommendProfessors/recommendByType.php`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ userID })
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+
+                const data = await response.json();
+                console.log('Received professor data:', data);
+                if (data.message === 'Quiz result not found for the given user ID.') {
+                    setQuizPromptVisible(true);
+                } else {
+                    setProfessorsData(data.professors);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const handleDepartmentChange = (event) => {
         setSelectedDepartment(event.target.value);
     };
 
-    const professorsData = [
-        {
-            name: "John Smith",
-            department: "Computer Science",
-            rating: 4.5,
-            pfppath: "john_smith.jpg",
-            fitrate:5,
-        },
-        {
-            name: "Emily Johnson",
-            department: "Mathematics",
-            rating: 4.0,
-            pfppath: "emily_johnson.jpg",
-            fitrate:9,
-        },
-        {
-            name: "Michael Williams",
-            department: "Physics",
-            rating: 3.8,
-            pfppath: "michael_williams.jpg",
-            fitrate:6.6,
-        },
-        {
-            name: "Sarah Brown",
-            department: "Chemistry",
-            rating: 4.2,
-            pfppath: "sarah_brown.jpg",
-            fitrate:10,
-        },
-        {
-            name: "David Davis",
-            department: "Biology",
-            rating: 3.9,
-            pfppath: "david_davis.jpg",
-            fitrate: 100,
+    const handleQuizPrompt = (response) => {
+        if (response === 'yes') {
+            navigate('/quizpage');
         }
-    ];
+    };
 
     const filteredProfessors = selectedDepartment
-        ? professorsData.filter(professor => professor.department === selectedDepartment)
+        ? professorsData.filter(professor =>
+            professor.Department.split(';').some(dept => dept.trim() === selectedDepartment)
+        )
         : professorsData;
 
     return (
         <div>
-            <NavBar/>
-        <div className="recommended-professors-page-content">
-            <h1 className="recommended-professors-page-title">Recommended Professors</h1>
-            <div>
-                <label htmlFor="departmentFilter">Filter by Department:</label>
-                <select id="departmentFilter" value={selectedDepartment} onChange={handleDepartmentChange}>
-                    <option value="">All Departments</option>
-                    {[...new Set(professorsData.map(professor => professor.department))].map((department, index) => (
-                        <option key={index} value={department}>{department}</option>
-                    ))}
-                </select>
-            </div>
-            <div className="recommended-professors-container">
-                {filteredProfessors.map((professor, index) => (
-                    <div key={index} className="recommended-professor-cards">
-                        <div className="recommended-professor-infos-container">
-                            <img src={professor.pfppath} alt="Professor" className="recommended-professor-img" onError={(e) => e.target.src = Default} />
-                            <div className="recommended-professor-infos">
-                                <h2 className="recommended-professor-name">{professor.name}</h2>
-                                <p className="recommended-professor-department">{professor.department}</p>
-                                <p className="recommended-professor-rating">Rating: {professor.rating + '/5'|| '-/5'}</p>
-                                <p className="recommended-professor-rating">Fitrate: {professor.fitrate +'%'|| '-%/100%'}</p>
-                            </div>
-                        </div>
+            <NavBar />
+            <div className="recommended-professors-page-content">
+                <h1 className="recommended-professors-page-title">Recommended Professors</h1>
+                {quizPromptVisible && (
+                    <div className="quiz-prompt">
+                        <p>It seems you haven't completed the quiz yet.</p>
+                        <p>Would you like to take the quiz to get personalized professor recommendations?</p>
+                        <button onClick={() => handleQuizPrompt('yes')}>Yes</button>
                     </div>
-                ))}
+                )}
+                {!quizPromptVisible && (
+                    <div>
+                        <label htmlFor="departmentFilter">Filter by Department:</label>
+                        <select id="departmentFilter" value={selectedDepartment} onChange={handleDepartmentChange}>
+                            <option value="">All Departments</option>
+                            {[...new Set(professorsData.flatMap(professor => professor.Department.split(';').map(dept => dept.trim())))].map((department, index) => (
+                                <option key={index} value={department}>{department}</option>
+                            ))}
+                        </select>
+                    </div>
+                )}
+                {!quizPromptVisible && (
+                    <div className="recommended-professors-container">
+                        {filteredProfessors.map((professor, index) => (
+                            <div key={index} className="recommended-professor-cards">
+                                <div className="recommended-professor-infos-container">
+                                    <img
+                                        src={importProfessorImage(professor.PfpPath.split('/').pop())}
+                                        alt="Professor"
+                                        className="recommended-professor-img"
+                                        onError={(e) => e.target.src = Default}
+                                    />
+                                    <div className="recommended-professor-infos">
+                                        <Link
+                                            to={{
+                                                pathname: `/professor/${professor.Name + '+' + professor.Department + '+' + professor.PfpPath.split('/').pop() + '+' + professor.ProfessorID}`
+                                            }}
+                                        >
+                                            {professor.Name}
+                                        </Link>
+                                        <p className="recommended-professor-department">{professor.Department}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
-        </div>
         </div>
     );
 };
